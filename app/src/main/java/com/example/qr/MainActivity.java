8cd16f24902bad5ca6f594c375906383215bb3b9
dj1sklero <60883106+dj1sklero@users.noreply.github.com>
2020-02-10 19:13:51 +0100

    16:42

--- app/src/main/java/com/example/qr/MainActivity.java
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -183,15 +183,8 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         Intent intent = new Intent(this, Activity_lista.class);
         startActivity(intent);
     }
-    public void tst() {
-        FloatingActionButton fab = findViewById(R.id.setting);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
+
+    public void setting(View view) {
+    }
 }
